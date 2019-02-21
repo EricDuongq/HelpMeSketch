4d19312a07862824f17961dbb2bc9311467bb6b1
Eric Duong <ericquangq@gmail.com>
2019-02-20 18:50:43 -0800

    Initial commit

---
+++ WebApi/WebApi/Models/ToDoCreate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models
+{
+    public class ToDoCreate
+    {
+        [Required]
+        public string Task { get; set; }
+    }
+}
